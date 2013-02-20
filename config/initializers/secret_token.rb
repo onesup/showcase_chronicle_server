b6b8720da18d7685948d4be578e17adb40afb917
Wonsup Lee <daul0212@gmail.com>
2013-02-20 17:33:13 +0900

    First Commit
    프로젝트 생성

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShowcaseChronicleServer::Application.config.secret_token = '4747a5cb0b49b84c560eebdf439543321ad1ec5c6b9acc3029d569384b7874d0b15031e286ce90e72510a94b11e572029a534c6b83458a5d8e774480aa9c6cb0'
