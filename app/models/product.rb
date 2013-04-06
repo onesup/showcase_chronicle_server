83d43b219d5a156d4df48f38c46bd1ba02871c34
Wonsup Lee <daul0212@gmail.com>
2013-04-06 16:44:43 +0900

    category select

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,4 @@
 class Product < ActiveRecord::Base
-  attr_accessible :brand, :code, :color, :comment, :name, :url
+  attr_accessible :brand, :code, :color, :comment, :name, :url, :category_id
   belongs_to :category
 end
