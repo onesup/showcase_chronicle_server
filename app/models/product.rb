d00c89f901462c3bc5c4972328db4bb04549bfd6
Wonsup Lee <daul0212@gmail.com>
2013-04-22 18:26:51 +0900

    fix category name and image

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,4 @@
 class Product < ActiveRecord::Base
-  attr_accessible :brand, :code, :color, :comment, :name, :url, :category_id
+  attr_accessible :brand, :code, :color, :comment, :name, :url, :category_id, :thumbnale_url
   belongs_to :category
 end
